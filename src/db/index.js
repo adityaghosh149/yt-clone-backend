21af36943a91b9b9087ee17952366cf134fa2967
Aditya Ghosh <ghoshaditya149@gmail.com>
2025-02-11 18:07:27 +0530

    improve console.log()

--- src/db/index.js
+++ src/db/index.js
@@ -7,10 +7,10 @@ const connectDB = async () => {
             `${process.env.MONGODB_URI}/${DB_NAME}`
         );
         console.log(
-            `MONGODB CONNECTED! DB HOST: ${connectionInstance.connection.host}`
+            `✅ MONGODB CONNECTED! 🚀 DB HOST: ${connectionInstance.connection.host}`
         );
     } catch (error) {
-        console.log("MONGODB CONNECTION FAILED!! ", error);
+        console.error("❌ MONGODB CONNECTION FAILED!!", error);
         process.exit(1);
     }
 };
--- src/index.js
+++ src/index.js
@@ -2,18 +2,17 @@ import dotenv from "dotenv";
 import app from "./app.js";
 import connectDB from "./db/index.js";
 
-
 dotenv.config({
     path: "./.env",
 });
 
 connectDB()
-.then(() => {
-    const PORT = process.env.PORT || 8080;
-    app.listen(PORT, () => {
-        `SERVER IS RUNNING ON PORT: ${PORT}`
+    .then(() => {
+        const PORT = process.env.PORT || 8080;
+        app.listen(PORT, () => {
+            console.log(`🚀 SERVER IS RUNNING ON PORT: ${PORT}`);
+        });
     })
-})
-.catch((err) => {
-    console.log("MONGODB CONNECTION FAILED!! ", err)
-})
+    .catch((err) => {
+        console.error("❌ MONGODB CONNECTION FAILED!!", err);
+    });
