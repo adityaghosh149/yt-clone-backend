898c54aa055085321eda2b12d46dcfc636ab704f
Aditya Ghosh <ghoshaditya149@gmail.com>
2025-02-14 16:14:17 +0530

    add optional verify jwt middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -34,4 +34,37 @@ const verifyJWT = asyncHandler(async (req, res, next) => {
     }
 });
 
-export { verifyJWT };
+const optionalVerifyJWT = asyncHandler(async (req, res, next) => {
+    try {
+        const accessToken =
+            req.cookies?.accessToken ||
+            req.header("Authorization")?.replace("Bearer ", "");
+
+        if (!accessToken) {
+            req.user = null;
+            return next();
+        }
+
+        const decodedToken = jwt.verify(
+            accessToken,
+            process.env.ACCESS_TOKEN_SECRET
+        );
+
+        const user = await User.findById(decodedToken?._id).select(
+            "-password -refreshToken"
+        );
+
+        if (!user) {
+            req.user = null;
+            return next();
+        }
+
+        req.user = user;
+        next();
+    } catch (error) {
+        req.user = null;
+        next();
+    }
+});
+
+export { optionalVerifyJWT, verifyJWT };
